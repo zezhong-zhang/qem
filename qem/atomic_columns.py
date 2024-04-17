6f5cea015c8619d5f2ad3fedcde8701dd8fb195f
Zezhong Zhang <jack.zezhong.zhang@gmail.com>
2024-04-17 17:41:39 +0200

    add atomic column class

---
+++ qem/atomic_columns.py
@@ -0,0 +1,9 @@
+import numpy as np
+from ase import Atoms
+from qem.periodic_table import chemical_symbols
+
+class AtomicColumns(Atoms):
+    def __init__(self, symbols, positions, scs=None, pbc=False):
+        super().__init__(symbols=symbols, positions=positions, pbc=pbc)
+        self.scs = scs
+
